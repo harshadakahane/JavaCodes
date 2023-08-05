f0e0b824b30d398c4a8dca07d241f52bc44c6c98
harshadakahane <harshadakahane281@gmail.com>
2023-08-05 21:55:23 +0530

    String == & equal operator

---
+++ string/string1.java
@@ -0,0 +1,9 @@
+class demo1{
+        public static void main(String [] args){
+                String str1 = new String("Harshada");
+                String str2 = new String("Harshada");
+		
+		System.out.println(str1 == str2);
+		System.out.println(str1.equals(str2));
+	}
+}
