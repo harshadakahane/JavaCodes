63e119aa4e17199c2d80e5abc8441b986242fed4
harshadakahane <harshadakahane281@gmail.com>
2023-08-05 12:04:56 +0530

    Collection with note

---
+++ Collection/collection.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class collection
+{
+	public static void main(String [] args)
+	{
+		List obj = new ArrayList();
+
+		obj.add(10);
+		obj.add(20);
+		obj.add(30);
+
+		System.out.println(obj);
+
+	}
+}
+
