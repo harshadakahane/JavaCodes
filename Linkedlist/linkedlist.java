427c65631f84348386e3011fe3091c3aa5dcff9a
harshadakahane <harshadakahane281@gmail.com>
2023-08-11 13:21:29 +0530

    Linkedlist demo

---
+++ Linkedlist/linkedlist.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Linkedlistdemo
+{
+	public static void main(String[] args)
+	{
+		LinkedList ll = new LinkedList();
+
+		ll.add(20);
+		ll.addFirst(45);
+		ll.addLast(56);
+
+		
+		System.out.println(ll);
+	}
+	
+}
+
