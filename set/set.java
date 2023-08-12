06401e7e0f63e287f566ad1f801fb146a6d3dc8a
harshadakahane <harshadakahane281@gmail.com>
2023-08-12 20:07:32 +0530

    hashset code

---
+++ set/set.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class setdemo{
+	public static void main(String[] args)
+	{
+		HashSet hs = new HashSet();
+		hs.add("kanha");
+		hs.add("amit");
+		hs.add("harshda");
+		hs.add("shivani");
+		hs.add("sumit");
+		hs.add("shree");
+		hs.add("neha");
+		System.out.println(hs);
+
+	}
+}
