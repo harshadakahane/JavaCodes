064b2412f4359c93fe13d1f11d5a870aeec5148b
harshadakahane <harshadakahane281@gmail.com>
2023-08-12 20:49:10 +0530

    Treeset demo

---
+++ set/Treeset.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Treesetdemo
+{
+	public static void main(String[] args)
+	{
+		TreeSet ts = new TreeSet();
+
+		ts.add("Amit");
+		ts.add("Harshada");
+		ts.add("Shivani");
+		ts.add("vaibhavi");
+
+		System.out.println(ts);
+	}
+}
+
+
