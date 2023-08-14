e7b347be4795fe9a15229c871790d91dc420d32d
harshadakahane <harshadakahane281@gmail.com>
2023-08-14 12:25:18 +0530

    Butterfly Pattern

---
+++ Pattern/ButterflyPattern.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class demo
+{
+       	public static void main(String[] args)
+	{
+
+		Scanner sc = new Scanner(System.in);
+
+                System.out.println("Enter no of n:");
+
+		int n = sc.nextInt();
+
+                //upper half
+		for(int i =1; i<=n; i++)
+		{
+			// 1st part
+			for(int j =1; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			//spaces
+			int spaces = 2 * (n-i);
+			for(int j =1; j<=spaces; j++)
+			{
+				System.out.print(" ");
+			}
+
+			//2nd part
+			for(int j =1; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+		
+
+		//lower half
+		
+		for(int i=n; i>=1; i--)
+		{
+			//1st part
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			//spaces 
+			int spaces = 2*(n-i);
+			for(int j=1; j<=spaces; j++)
+			{
+				System.out.print(" ");
+			}
+			//2nd part
+			for(int j =1; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+						         				
