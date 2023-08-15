93d8e222841ff495b746b6f47481be7c913e02c9
harshadakahane <harshadakahane281@gmail.com>
2023-08-15 14:11:03 +0530

    Daimond  Pattern

---
+++ Pattern/Daimondpattern.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class demo
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter value of n:");
+
+		int n = sc.nextInt();
+
+		for(int i=1; i<=n; i++)
+		{
+			for(int j=1; j<=n-i; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j =1; j<=2*i-1; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+
+		//lower half 
+		for(int i=n; i>=1; i--)
+	       	{
+                	for(int j=1; j<=n-i; j++)
+			{
+			System.out.print(" ");
+			}
+			for(int j =1; j<=2*i-1; j++)
+			{
+                	System.out.print("*");
+			}
+                        System.out.println();
+               }
+
+	}
+}
