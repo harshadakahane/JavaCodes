cf58b2a0f67b61ff6d65757b70839b95d12ab661
harshadakahane <harshadakahane281@gmail.com>
2023-09-12 19:22:06 +0530

    Print n natural no from 1 to 5

---
+++ Recursion/recursion2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class recursion
+{
+	public static void printNumb(int n)
+	{
+		if(n==6)
+		{
+			return;
+		}
+		System.out.println(n);
+		printNumb(n+1);
+	}
+	public static void main(String[] args)
+	{
+		int n =1;
+		printNumb(n);
+	}
+	
+}
+
+
