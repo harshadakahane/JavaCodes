294f21b7ded60d6327d72a62b08bd5b0022aedfa
harshadakahane <harshadakahane281@gmail.com>
2023-09-17 13:01:51 +0530

    Search an element in an array

---
+++ Array1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Array
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter size of an array:");
+
+		int n= sc.nextInt();
+		
+		int[] arr = new int[n];
+		System.out.println("Enter elements in an array:");
+
+		for(int i=0; i<n; i++)
+		{
+			arr[i]=sc.nextInt();
+			
+		}
+		System.out.println("Enter the value to be searched:");
+
+		int value = sc.nextInt();
+
+
+		for(int i =0; i<arr.length; i++)
+		{
+			if(arr[i] == value)
+			{
+				System.out.println("The index of element " +value+" is :"+i);
+			}
+		}
+
+	
+	}
+}
+
