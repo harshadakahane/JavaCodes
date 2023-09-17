fd999262128991e34fe994d1547953980019c599
harshadakahane <harshadakahane281@gmail.com>
2023-09-17 14:48:20 +0530

    Find minimum and maximum element in an array

---
+++ ArrayBasic/Array2.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class harshada
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of an array:");
+
+		int n = sc.nextInt();
+
+		if(n<=0)
+		{
+			System.out.println("Invalid input. The array size must be greater than 0");
+	        return;
+		}
+
+
+		int arr[]  = new int[n];
+
+		System.out.println("Enter the elements in an array:");
+		for(int i=0; i<n; i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		sc.close();
+
+		int min = arr[0];
+		int max = arr[0];
+
+		for(int i =1; i<n; i++)
+		{
+		
+			if(arr[i]<min)
+			{
+			min=arr[i];
+			}
+			if(arr[i]>max)
+			{
+			max=arr[i];
+			}
+		}
+
+		System.out.println("Maximum element is "+max);
+		System.out.println("Minimum element is "+min);
+
+
+	}
+}
+
